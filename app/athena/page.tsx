b8503eca0d683d0536dfb8d1bb56ba75c0796ee9
Yonas Adiel Wiguna <yonazadielwiguna@gmail.com>
2024-01-08 21:59:28 +0800

    Add old posts

--- app/athena/page.tsx
+++ app/athena/page.tsx
@@ -116,18 +116,22 @@ interface IndexProps {
 
 const Index = (props: IndexProps) => {
   const { allPosts } = props;
+  const postsByYear: { [year: string]: Post[] } = {}
+  for (let i = 0; i < allPosts.length; i++) {
+    const year = allPosts[i].date.getFullYear()
+    if (!(year in postsByYear)) { postsByYear[year] = [] }
+    postsByYear['' + allPosts[i].date.getFullYear()].push(allPosts[i])
+  }
   return (
     <div className="pt-8">
-      {allPosts
-        .map((p) => p.date.getFullYear())
-        .filter((p, i, a) => a.indexOf(p) === i)
-        .map((yr) => allPosts.filter((p) => p.date.getFullYear() === yr))
-        .sort((a, b) => a > b ? 1 : -1)
+      {Object.keys(postsByYear)
+        .map((year) => ({ year: year, posts: postsByYear[year] }))
+        .sort((a, b) => a.year < b.year ? 1 : -1)
         .map((pl) => (
-          <div key={pl[0].date.getFullYear()} className="pb-3">
-            <h4 className="font-bold">{pl[0].date.getFullYear()}</h4>
+          <div key={pl.year} className="pb-3">
+            <h4 className="font-bold">{pl.year}</h4>
             <div>
-              {pl.sort((a, b) => a.date < b.date ? 1 : -1).map((p) =>
+              {pl.posts.sort((a, b) => a.date < b.date ? 1 : -1).map((p) =>
                 <Link href={`/athena?slug=${p.slug}`} className="block hover:underline mb-3" key={p.slug}>{p.title}</Link>
               )}
             </div>
